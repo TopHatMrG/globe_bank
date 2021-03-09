4b2db86a4be2aa14e33dcd9832d8a69ab1a3cd19
Gavin <contactgavinharris@gmail.com>
2021-03-08 19:06:08 -0500

    Finished chapter 2 Build Web Pages with PHP

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -4,6 +4,10 @@
 
   $id = $_GET['id'] ?? '1';
 
-  echo $id;
+  echo h($id);
 
 ?>
+
+<a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br />
+<a href="show.php?=company<?php echo u('Widgets&More'); ?>">Link</a><br />
+<a href="show.php?=query<?php echo u('!#*?'); ?>">Link</a><br />
